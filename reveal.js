35eba639809f92221e097316bd63cd32e22a9721
MAYCON <111540660+MAYCONARAUJONASCIMENTO@users.noreply.github.com>
2024-04-24 21:31:24 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,30 @@
+
+ScrollReveal().reveal('h1 , p , .butt ',
+{
+    duration:2000,
+    distance: '90px',
+    
+});
+
+ScrollReveal().reveal( '.gta',
+{
+    duration:2000,
+    distance: '90px',
+    daley:500
+}
+
+);
+ScrollReveal().reveal('h2 , h3 , h4 , h5 ',
+{
+    duration:3000,
+    distance: '30px',
+    daley:500
+});
+
+ScrollReveal().reveal('h6',
+{
+    duration:2500,
+    distance: '90px',
+    daley:500
+});
+
